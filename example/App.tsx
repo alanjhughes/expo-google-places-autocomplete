f1665202c73b1cc803586555e70f62fc1c8d0c3a
Alan Hughes <hughesno1@gmail.com>
2022-10-02 00:06:17 +0100

    Fix example foleder

---
+++ example/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, View, Text, Platform } from "react-native";
+
+import { GooglePlacesAutocomplete } from "expo-google-places-autocomplete";
+import {
+  PlacesError,
+  PlaceDetails,
+} from "expo-google-places-autocomplete/types";
+import { API_KEY } from "@env";
+
+export default function App() {
+  const onSearchError = React.useCallback((error: PlacesError) => {
+    console.log(error);
+  }, []);
+
+  const onPlaceSelected = React.useCallback((place: PlaceDetails) => {
+    console.log(place);
+  }, []);
+
+  return (
+    <View style={styles.screen}>
+      <View style={{ marginTop: Platform.OS === "ios" ? 44 : 0 }}>
+        <Text style={styles.title}>Google Places Autocomplete</Text>
+        <GooglePlacesAutocomplete
+          apiKey={API_KEY}
+          requestConfig={{ countries: ["IE"] }}
+          onPlaceSelected={onPlaceSelected}
+          onSearchError={onSearchError}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    height: "100%",
+    backgroundColor: "rgba(211, 211, 211, 0.3)",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 18,
+    marginVertical: 10,
+  },
+});
